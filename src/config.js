98390bda20cbb8f22181c2bdea14b2d78797a6c7
trevorlomba <trevorlomba@gmail.com>
2024-11-11 15:47:45 -0500

    omg3

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-  apiKey: window.REACT_APP_OPENAI_API_KEY || 'default-or-empty-value'
+  apiKey: process.env.NODE_ENV === 'development' 
+    ? process.env.REACT_APP_OPENAI_API_KEY 
+    : window.REACT_APP_OPENAI_API_KEY || ''
 };
 
 export default config;
