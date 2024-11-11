45ce87de85e13283395981c8cb885d23b9205060
trevorlomba <trevorlomba@gmail.com>
2024-11-11 15:59:00 -0500

    Fix config loading and environment variable handling

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   apiKey: process.env.NODE_ENV === 'development' 
     ? process.env.REACT_APP_OPENAI_API_KEY 
-    : window.REACT_APP_OPENAI_API_KEY || ''
+    : (window.REACT_APP_OPENAI_API_KEY || '')
 };
 
 export default config;
