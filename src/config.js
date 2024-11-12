a17f5ded0530e6ef1bda0cd488bb9c083169a746
trevorlomba <trevorlomba@gmail.com>
2024-11-11 20:45:39 -0500

    continuing-to-troubleshoot

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  apiKey: process.env.NODE_ENV === 'development' 
-    ? process.env.REACT_APP_OPENAI_API_KEY 
-    : window.REACT_APP_OPENAI_API_KEY
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 };
 
 export default config;
